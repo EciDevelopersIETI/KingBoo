eb3e9fa57320ec2745b38fbb0ca3a8046e18c6a2
fernando <f9830@hotmail.com>
2020-08-20 03:23:51 -0500

    primer avance de Front :wolf:

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import Login from './components/login';
+import SignUp from './components/signUp';
+import Home from './components/home';
+function App() {
+
+  const LoginView = () => <Login />;
+  const SignUpView = () => <SignUp />;
+  const home = () => <Home />
+
+  return (
+    <Router>
+    <div className="App">
+      <div>
+        <Route exact path="/" component={LoginView} />
+        <Route exact path="/sign-up" component={SignUpView} />
+        <Route exact path="/home" component={home} />
+      </div>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
