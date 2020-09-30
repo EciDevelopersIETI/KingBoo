4c1f159a7bcb89ab30f34d79ba944985d4d5c9c3
Luis Alejandro Jaramillo <luisalejandrojaramillo@hotmail.com>
2020-09-30 03:58:17 -0500

    funcionalidad completa de add provider con mejoras en las vistas

--- src/api/createUser.js
+++ src/api/createUser.js
@@ -10,17 +10,18 @@ const newUser = user => {
 	console.log('llega auiiiiiiiiiiiiii');
 	console.log(user);
     axios.post('https://kingboooback.herokuapp.com/users/newuser', {
-             email: user.email,
-             password: user.password,
-             telefono: user.telefono,
-             userName: user.firstName
+            email: user.email,
+            password: user.password,
+            telefono: user.telefono,
+            userName: user.firstName,
+            provider: user.provider
+
      })
-	
       .then(function (response) {
         alert("Creacion exitosa !!!!!!!!!!!")
       })
       .catch(function (error) { 
-		console.log(error);
+		    console.log(error);
         alert("Error al crear USER");
       });
 
