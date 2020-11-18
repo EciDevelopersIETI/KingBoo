b49e6a895dcf108937eaa882cbcc9a666cbf3e36
Luis Alejandro <36908783+luisalejandrojaramillo@users.noreply.github.com>
2020-11-18 02:35:09 -0500

    Imagen de fernan :trollface:

--- src/components/home.js
+++ src/components/home.js
@@ -73,7 +73,7 @@ const Home = () => {
 
         <div>
           <img className = 'img-icons'
-                src="https://media-exp1.licdn.com/dms/image/C4E35AQFKrEd0-kxNCw/profile-framedphoto-shrink_400_400/0?e=1605333600&v=beta&t=7eUo-b3-D9V_LzTQJ91JlC7aScP6cy81xCWasxd1La8" />
+                src="https://media-exp1.licdn.com/dms/image/C4E35AQFKrEd0-kxNCw/profile-framedphoto-shrink_800_800/0?e=1605772800&v=beta&t=3Zcp-xJhyi3ZeWg7JEIyj9D1HROUJNrjHuP-ZBTm18M" />
           <div>
             <p>
               <b>Nombre:</b> <span>Fernando Barrera</span>{" "}
